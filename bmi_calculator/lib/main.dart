8b60d4d8b97187ebb794b02a53f74881e3943f34
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2020-02-24 17:00:08 -0300

    modify theme of app

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,18 +1,19 @@
-import 'package:bmi_calculator/home.dart';
+import 'package:bmi_calculator/input_page.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(BMICalculator());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
-      home: Home(),
+      home: InputPage(),
+
+      debugShowCheckedModeBanner: false,
     );
   }
 }
